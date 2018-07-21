f96adc9fb2baec98573fadce1df6b46cd4d3151a
Hassan Ahamed <hassan.ahamed@philips.com>
2018-07-21 20:28:09 +0530

    1st Push..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AdminViewComponent } from './admin-view/admin-view.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'marketplace', component: MarketplaceComponent },
+  { path: 'adminview', component: AdminViewComponent }
+];
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MarketplaceComponent,
+    AdminViewComponent
   ],
   imports: [
     BrowserModule
+    , FormsModule
+    , ReactiveFormsModule
+    , RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
